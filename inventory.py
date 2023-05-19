dca53059595eeaca2862dec9e911ddb2ed1bd7f0
darthturska <aatu.kajasto@gmail.com>
2023-05-19 12:02:23 +0300

    inventory

--- inventory.py
+++ inventory.py
@@ -10,13 +10,18 @@ class ItemStack:
         self.item_count = item_count
         self.rect = 0
 
+    def draw(self, font, x, y, surf):
+        surf.blit(self.item.img, (x, y))
+        if self.item_count > 1:
+            font.render_to(surf, (x, y), str(self.item_count), (255, 255, 255))
 
 
 class Inventory:
     def __init__(self, width, height, font: ft.Font):
         self.width = width
         self.height = height
-        self.items = [ItemStack(t.DIRT, i) for i in range(width * height)]
+        self.items = [ItemStack(t.DIRT, i + 1) for i in range(width * height)]
+        self.selected_index = None
 
         self.cell_margin = 4
         self.cell_padding = 2
@@ -34,13 +39,16 @@ class Inventory:
         self.item_rects = self.generate_rects()
         self.surface = pg.Surface(self.rect.size)
 
-
     def generate_rects(self):
         item_rects = []
         for i in range(self.width * self.height):
             x = i % self.width * self.cell_total + self.rect_padding
             y = i // self.width * self.cell_total + self.rect_padding
-            item_rects.append(pg.Rect(x, y, TILE_SIZE + self.cell_padding, TILE_SIZE + self.cell_padding))
+            item_rects.append(
+                pg.Rect(
+                    x, y, TILE_SIZE + self.cell_padding, TILE_SIZE + self.cell_padding
+                )
+            )
 
         return item_rects
 
@@ -48,13 +56,13 @@ class Inventory:
         if event.type == pg.MOUSEBUTTONDOWN:
             if event.button == 1:
                 if self.rect.collidepoint(*event.pos):
-                    #translate the event pos
+                    # translate the event pos
                     rect = pg.Rect(event.pos, (1, 1)).move(-self.rect.x, -self.rect.y)
                     collision = rect.collidelist(self.item_rects)
 
-                    #collision is -1 when there aren't any collisions
+                    # collision is -1 when there aren't any collisions
                     if collision != -1:
-                        itemstack =  self.items[collision]
+                        itemstack = self.items[collision]
                         if grabbed_item is None:
                             self.items[collision] = None
                             return itemstack
@@ -62,20 +70,24 @@ class Inventory:
                             if itemstack is None:
                                 self.items[collision] = grabbed_item
                             else:
-                                itemstack.item_count+=grabbed_item.item_count
+                                itemstack.item_count += grabbed_item.item_count
                             return None
         return grabbed_item
 
-
     def draw(self, surf: pg.Surface):
         self.surface.fill("#181425")
-        pg.draw.rect(self.surface, "#262b44", ((0,0), self.rect.size), width= 1)
+        pg.draw.rect(self.surface, "#262b44", ((0, 0), self.rect.size), width=1)
         for i, rect in enumerate(self.item_rects):
             pg.draw.rect(self.surface, "#262b44", rect)
+            if self.selected_index == i:
+                pg.draw.rect(self.surface, "#feae34", rect, width=1)
             item_stack = self.items[i]
             if item_stack is not None:
+                item_stack.draw(
+                    self.font,
+                    rect.x + self.cell_padding // 2,
+                    rect.y + self.cell_padding // 2,
+                    self.surface,
+                )
 
-                self.surface.blit(item_stack.item.img, (rect.x+ self.cell_padding//2,  rect.y+ self.cell_padding//2))
-                self.font.render_to(self.surface, (rect.left + 1, rect.top + 1), str(item_stack.item_count), (255, 255, 255))
-  
         surf.blit(self.surface, self.rect)
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import ez_profile
-
 import enum
 import pygame as pg
 import pygame.freetype as ft
+import numpy
+
 pg.init()
 
 from settings import *
@@ -17,6 +17,7 @@ class State(enum.Enum):
     INVENTORY = 0
     GAME = 1
 
+
 def main():
     state = State.GAME
     clock = pg.Clock()
@@ -24,44 +25,49 @@ def main():
     font = ft.Font("textures/scientifica/bdf/scientifica-11.bdf")
     inventory = Inventory(9, 4, font)
     hotbar = Inventory(9, 1, font)
-    hotbar.rect.bottom = HEIGHT-10
+    hotbar.selected_index = 0
+    hotbar.rect.bottom = HEIGHT - 10
     grabbed_item = None
     background = win.copy()
     while True:
         dt = clock.tick() / 1000
         if state == State.GAME:
             world.update(dt)
+
             world.draw(win)
-        elif state == State.INVENTORY:
+            hotbar.draw(win)
+
+        if state == State.INVENTORY:
             win.blit(background, (0, 0))
             inventory.draw(win)
-        hotbar.draw(win)
-        if grabbed_item:
-            mpos = pg.Vector2(pg.mouse.get_pos())
-            win.blit(grabbed_item.item.img, (mpos - pg.Vector2(TILE_SIZE/2)))
+            hotbar.draw(win)
+            if grabbed_item:
+                mpos = pg.Vector2(pg.mouse.get_pos())
+                grabbed_item.draw(font, *(mpos - pg.Vector2(TILE_SIZE / 2)), win)
+
         pg.display.update()
-        
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 raise SystemExit
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_ESCAPE:
-                    if state== State.INVENTORY:
-                        grabbed_item = None
+                    if state == State.INVENTORY:
                         state = State.GAME
-                    else: 
+                    else:
                         state = State.INVENTORY
                         background = win.copy()
 
                 if event.unicode in "123456789":
-                    print(event.unicode)
+                    hotbar.selected_index = int(event.unicode) - 1
+
+            if event.type == pg.MOUSEWHEEL:
+                hotbar.selected_index = (
+                    hotbar.selected_index + numpy.sign(event.y)
+                ) % 9
             if state == State.INVENTORY:
                 for ui in [inventory, hotbar]:
                     grabbed_item = ui.handle_event(event, grabbed_item)
-                    if grabbed_item:
-                        break
-
 
 
 if __name__ == "__main__":
